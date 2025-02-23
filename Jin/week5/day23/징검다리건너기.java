5bedcbf147f266213df114e294d3ea159c49d720
JIN <o3ouuu@inu.ac.kr>
2025-02-23 23:23:37 +0900

    feat: 징검다리건너기 풀이

---
+++ Jin/week5/day23/징검다리건너기.java
@@ -0,0 +1,37 @@
+package Jin.week5.day23;
+
+public class 징검다리건너기 {
+    public int solution(int[] stones, int k) {
+        int left = 1, right = 200000000, answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isPossible(stones, k, mid)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isPossible(int[] stones, int k, int mid) {
+        int zero = 0;
+
+        for (int stone : stones) {
+            if (stone - mid < 0) {
+                zero++;
+
+                if (zero >= k) {
+                    return false;
+                }
+            } else {
+                zero = 0;
+            }
+        }
+        return true;
+    }
+}
