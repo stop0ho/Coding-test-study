28195c00771c84da06e8ba928ca64cde3341f7f6
JIN <o3ouuu@inu.ac.kr>
2025-02-28 12:53:14 +0900

    feat: 정수 삼각형 문제 풀이

---
+++ Jin/week6/day30/정수삼각형.java
@@ -0,0 +1,31 @@
+package Jin.week6.day30;
+
+public class 정수삼각형 {
+    public int solution(int[][] triangle) {
+        int height = triangle.length;
+        int[][] dp = new int[height][height];
+
+        dp[0][0] = triangle[0][0];
+        for (int i = 1; i < height; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    // 가장 왼쪽의 경우
+                    dp[i][j] = triangle[i][j] + dp[i - 1][j];
+                } else if (j == i) {
+                    // 가장 오른쪽의 경우
+                    dp[i][j] = triangle[i][j] + dp[i - 1][j - 1];
+                } else {
+                    // 나머지 경우
+                    dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        int MAX_VALUE = 0;
+        for (int i = 0; i < height; i++) {
+            MAX_VALUE = Math.max(MAX_VALUE, dp[height - 1][i]);
+        }
+
+        return MAX_VALUE;
+    }
+}
